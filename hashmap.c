4a9a96a0ac2a35c319a7187e055250f58c2b043a
Your Name <you@example.com>
2025-04-13 17:09:26 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -57,7 +57,7 @@ HashMap * createMap(long capacity) {
     mapa->capacity = capacity ;
     mapa->size = 0 ;
     mapa->buckets = (Pair **) malloc(sizeof(Pair *)) ;
-    (*mapa)->buckets = (Pair *) malloc(sizeof(Pair)) ;
+    mapa->buckets[0] = (Pair *) malloc(sizeof(Pair)) ;
     return mapa;
 }
 
