1c055a807ab8db2c7dc2c38c8032428f5440ee69
Your Name <you@example.com>
2025-04-13 17:34:23 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -44,7 +44,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     if (capacidad == map->size) return ;
     printf("Primer debug") ;
     for(int i = 0 ; i < capacidad ; i++){
-        if (strcmp(map->buckets[i]->key, key) == 0) return ;
+        if (strcmp(map->buckets[i]->key, (char * Pair)key) == 0) return ;
     }
     printf("Segundo debug") ;
     int posicion = hash(key, capacidad) ;
